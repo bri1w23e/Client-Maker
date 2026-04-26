d1ef09d96186df95b2e289538efca5ad056e7167
kanndbeans <148579779+bri1w23e@users.noreply.github.com>
2026-04-26 01:22:54 -0400

    Add ModuleManager class for module management
    Implemented ModuleManager to manage game modules.

---
+++ src/game/java/net/minecraft/client/module/ModuleManager.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModuleManager {
+
+    public static List<Module> modules = new ArrayList<>();
+
+    public ModuleManager() {
+        // Add modules here
+        modules.add(new KillAura());
+        modules.add(new Fly());
+        modules.add(new Fullbright());
+    }
+
+    public static List<Module> getModulesByCategory(Category c) {
+        List<Module> list = new ArrayList<>();
+        for(Module m : modules) {
+            if(m.getCategory() == c) list.add(m);
+        }
+        return list;
+    }
+}
